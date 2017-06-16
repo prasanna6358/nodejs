b423a4130cd7f16fd62a1df397ec6906c3fb39b6
erperp <erperp@prasanna.billa@halcyontek.com>
2017-06-16 16:52:10 +0530

    hiring app changes

--- hiringtest/app.js
+++ hiringtest/app.js
@@ -12,19 +12,24 @@ var db = new Db('HireProcessDb', new Server('localhost', 27017));
 
 app.get('/',function(req,res)
 {
-	res.sendFile( __dirname +'/index.html');
+  res.sendFile( __dirname +'/index.html');
 });
 
-app.get('/admin',function(req,res)
+app.get('/user',function(req,res)
 {
-    res.sendFile(__dirname +'/admin.html');
+    res.sendFile(__dirname +'/user.html');
 });
+app.get('/admin', function(req,res)
+{
+  res.sendFile(__dirname+'/admin.html')
+})
 
-io.on('connection', function(socket){
+io.on('connection', function(socket) {
+
   console.log('A user connected '+socket.id);
 
+    setTimeout(function(){
 
-   setTimeout(function(){
     socket.send('Now you are connected to the server!!!');
   }, 4000);
 
@@ -34,12 +39,47 @@ io.on('connection', function(socket){
     
     db.open(function(err, db) {
     var collection = db.collection("HireProcess");
+    collection.find({userEmail:data._userEmail}).toArray(function (err, emailresult) {
+    
+    console.log(emailresult);
+    console.log('insede find sendemail'+data._userEmail);
+    if(emailresult.length>0){
+    console.log('user email found '+emailresult[0].userEmail);
+    var existemail = emailresult[0].userEmail;
+    if(data._userEmail==existemail)
+    {
+      console.log('user exist in database');
+      socket.emit('sendConfirmation','you have already submitted your answers with the email id '+emailresult[0].userEmail);
+
+    }
+  }
+    else{
     collection.insert([{username:data._userName,userEmail:data._userEmail,questionAnswers:data._questionAnswersData}], function(err, result) {
     assert.equal(null, err);
+    console.log('insert');
     console.log(result);
-        })
+    console.log('user not exist in database');
+    socket.emit('sendConfirmation','You have submitted your answers successfully');
+            })
+        }
      })
+    db.close();
+  })
   });
+
+      socket.on('adminsendemail',function(data) 
+           {
+              console.log(data);
+              db.open(function(err, db) {
+              var collection = db.collection('HireProcess');
+              collection.find({userEmail:data.email}).toArray(function (err, result) {
+                console.log(result);
+                socket.emit('adminuserdata',{result})
+               })
+              db.close();
+             })
+            });
+
   socket.on('disconnect', function () {
     console.log('A user disconnected '+socket.id);
 
